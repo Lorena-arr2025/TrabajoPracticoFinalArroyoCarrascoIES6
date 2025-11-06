a97aba283d7134858e2dc2f2812a8432868f04c0
Lorena-arr2025 <arrnan846@gmail.com>
2025-11-05 22:09:17 -0300

    Inicialiazndo el gran proyecto

---
+++ src/main/java/edu/ar/easymover/EasymoverApplication.java
@@ -0,0 +1,13 @@
+package edu.ar.easymover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasymoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasymoverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/ar/easymover/EasymoverApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ar.easymover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasymoverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
