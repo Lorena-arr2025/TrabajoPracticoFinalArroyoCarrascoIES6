fbe4ce8e6bb01b9091a4b96af2a875f3a008d415
Mirta <mirtaleonar@gmail.com>
2025-11-12 19:23:58 -0300

    Creación de atributos y constructores en clase Vehicle

--- src/main/java/edu/ar/easymover/model/Vehicle.java
+++ src/main/java/edu/ar/easymover/model/Vehicle.java
@@ -1,7 +1,31 @@
 package edu.ar.easymover.model;
 
 public class Vehicle {
+    // Atributos
+    private String patente;
+    private String marca;
+    private String color;
+    private String fechaFabricacion;
+    private String provinciaOrigen;
+    
 
-}
+    // Constructores---
+    
+    // Constructor por defecto
+    public Vehicle() {
+    
+} 
+    // constructores parametrizados
+public Vehicle(String patente, String marca, String color, String fechaFabricacion, String provinciaOrigen) {
+        this.patente = patente;
+        this.marca = marca;
+        this.color = color;
+        this.fechaFabricacion = fechaFabricacion;
+        this.provinciaOrigen = provinciaOrigen;
+
+    }    
+    // Getters y Setters 
+
+} 
     
 
