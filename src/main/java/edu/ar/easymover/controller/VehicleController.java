e1d61a600028bb1a0c199bb964608f279d2b23fe
Mirta <mirtaleonar@gmail.com>
2025-11-14 04:18:08 -0300

    Agrego VehicleController con mapeo básico

--- src/main/java/edu/ar/easymover/controller/VehicleController.java
+++ src/main/java/edu/ar/easymover/controller/VehicleController.java
@@ -1,5 +1,20 @@
-package edu.ar.easymover.controller;
+package edu.ar.easymover.controller; 
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/vehicles")
 public class VehicleController {
-    
+
+    @GetMapping
+    public String getVehicles() {
+        return "vehicles"; // Devuelve el nombre de la vista vehíulo
+    }
 }
+
+
+
+ 
+
